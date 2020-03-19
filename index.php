fc50f8a60f9a2bd5736dbe8668c8dc95bcd533f8
xellus <braden.coates@hotmail.com>
2020-03-19 14:41:34 -0400

    Added a home page that explains the website use and technology

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300|Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+    <?php require_once ('header.php'); ?>
+    <main>
+      <h2>About this website</h2>
+      <p>This website is a project for PHP class. It allows users to review certain restaurants, leaving a star rating as well.</p>
+      <h2>How it was built</h2>
+      <p>This website is currently running on AWS' appache servers, using a MySQL database to store all reviews. PHP runs the backend of the website, providing most of the funcitonality while html and css provide the look and feel of it.</p>
+    </main>
+  </body>
+</html>
